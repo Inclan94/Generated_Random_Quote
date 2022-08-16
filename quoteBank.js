e22071651622b25fa1bfc488e2133b62f89eb2d4
Inclan <inclansini@gmail.com>
2022-08-15 17:28:57 -0700

    random quote generator

---
+++ quoteBank.js
@@ -0,0 +1,151 @@
+const quoteBank = [
+    {
+        quote: "If you owe the bank $100 that's your problem. If you owe the bank $100 million, that's the bank's problem.",
+        author: "J. Paul Gett"
+    },
+
+    {
+        quote: "Your diet is a bank account. Good food choices are good investments.",
+        author: "Bethenny Frankel"
+    },
+    
+    {
+        quote: "I sincerely believe that banking establishments are more dangerous than standing armies, and that the principle of spending money to be paid by posterity, under the name of funding, is but swindling futurity on a large scale.",
+        author: "Thomas Jefferson "
+    },
+
+    {
+        quote: "Everyday is a bank account, and time is our currency. No one is rich, no one is poor, we've got 24 hours each.",
+        author: "Christopher Rice "
+    },
+
+    {
+        quote: "It is well enough that people of the nation do not understand our banking and monetary system, for if they did, I believe there would be a revolution before tomorrow morning.",
+        author:"Henry Ford "
+    },
+
+    {
+        quote: "He tried to read an elementary economics text; it bored him past endurance, it was like listening to somebody interminably recounting a long and stupid dream. He could not force himself to understand how banks functioned and so forth, because all the operations of capitalism were as meaningless to him as the rites of a primitive religion, as barbaric, as elaborate, and as unnecessary. In a human sacrifice to deity there might be at least a mistaken and terrible beauty; in the rites of the moneychangers, where greed, laziness, and envy were assumed to move all men's acts, even the terrible became banal.",
+        author: "Ursula K. Le Guin, The Dispossessed "
+    },
+
+    {
+        quote: "Rather than justice for all, we are evolving into a system of justice for those who can afford it. We have banks that are not only too big to fail, but too big to be held accountable.",
+        author: "Joseph E. Stiglitz"
+    },
+
+    {
+        quote: "I believe that banking institutions are more dangerous to our liberties than standing armies.",
+        author: "Thomas Jefferson"
+    },
+
+    {
+        quote: "Gringotts was the safest place in the world for something you wanted to hide — except perhaps Hogwarts.",
+        author: " J.K. Rowling, Harry Potter and the Sorcerer's Stone "
+    },
+
+    {
+        quote: "The drug war is a total scam, prescription drugs kill 300K a year, while marijuana kills no one, but they spend billions/year 'fighting' it, because pot heads make for good little slaves to put into private prisons, owned by the banks who launder the drug money, and it's ALL DOCUMENTED.",
+        author: "Alex E. Jones"
+    },
+
+    {
+        quote: "When they took a young man into Tellson's London house, they hid him somewhere till he was old. They kept him in a dark place, like a cheese, until he had the full Tellson flavour and blue-mould upon him. Then only was he permitted to be seen, spectacularly poring over large books, and casting his breeches and gaiters into the general weight of the establishment.",
+        author: "Charles Dickens, A Tale of Two Cities "
+    },
+
+    {
+        quote: "The rich run a global system that allows them to accumulate capital and pay the lowest possible price for labour. The freedom that results applies only to them. The many simply have to work harder, in conditions that grow ever more insecure, to enrich the few. Democratic politics, which purports to enrich the many, is actually in the pocket of those bankers, media barons and other moguls who run and own everything.",
+        author: "Charles Moore"
+    },
+
+    {
+        quote: "My bank must stop trying to sell me identity theft protection. You know why I expect you to protect my money? Because you're a bank.",
+        author: "Bill Maher"
+    },
+
+    {
+        quote: "Drive-in banks were established so most of the cars today could see their real owners.",
+        author: "E. Joseph Cossman"
+    },
+
+    {
+        quote: "If we were not impressed by job titles, suits, and jargon, we would demand that financial advisors show us their personal bank statements before they tell us what we could or should do with our own money.",
+        author: "Mokokoma Mokhonoana"
+    },
+
+    {
+        quote: "Banks are nothing but old fashioned money lenders. They encourage you to borrow, to get in debt, and when you can't pay back the loan they take your home away. At least a money lender only breaks your legs.",
+        author: "Karl Wiggins"
+    },
+
+    {
+        quote: "The banking institutions, my friends, are sailing under false colours. They are running with the hare and hunting with the hounds. And we should never forget it.",
+        author: "Karl Wiggins"
+    },
+
+    {
+        quote: "Myth: US housing market is in recovery. Fact: Big banks have been hiding their bloated home inventory, seized by virtue of home foreclosures.",
+        author: "Ziad K. Abdelnour"
+    },
+
+    {
+        quote: "A bank is a place that will lend you money if you can prove that you don't need it.",
+        author: "Bob Hope"
+    },
+
+    {
+        quote: "Money doesn't mean anything to me. I've made a lot of money, but I want to enjoy life and not stress myself building my bank account. I give lots away and live simply, mostly out of a suitcase in hotels. We all know that good health is much more important. ",
+        author: "Keanu Reeves"
+    },
+
+    {
+        quote: "I trust bitcoin more than I trust my bank. ",
+        author: "Adam Draper"
+    },
+
+    {
+        quote: "It is easier to rob by setting up a bank than by holding up a bank clerk. ",
+        author: "Bertolt Brecht"
+    },
+
+    {
+        quote: "A U.S. dollar is an IOU from the Federal Reserve Bank. It's a promissory note that doesn't actually promise anything. It's not backed by gold or silver. Share this Quote",
+        author: "P. J. O'Rourke"
+    },
+
+    {
+        quote: "Don't bring your need to the marketplace, bring your skill. If you don't feel well, tell your doctor, but not the marketplace. If you need money, go to the bank, but not the marketplace. ",
+        author: "Jim Rohn"
+    },
+
+    {
+        quote: "I saw a bank that said '24 Hour Banking', but I don't have that much time. ",
+        author: "Steven Wright"
+    },
+
+    {
+        quote: "If only God would give me some clear sign! Like making a large deposit in my name at a Swiss bank. ",
+        author: "Woody Allen"
+    },
+
+    {
+        quote: "The world is governed by institutions that are not democratic - the World Bank, the IMF, the WTO. ",
+        author: "Jose Saramago"
+    },
+
+    {
+        quote: "They say Yogi Berra is funny. Well, he has a lovely wife and family, a beautiful home, money in the bank, and he plays golf with millionaires. What's funny about that? ",
+        author: "Casey Stengel"
+    },
+
+    {
+        quote: "Anytime four New Yorkers get into a cab together without arguing, a bank robbery has just taken place. ",
+        author: "Johnny Carson"
+    },
+
+    {
+        quote: "Money looks better in the bank than on your feet.",
+        author: "Sophia Amoruso"
+    }
+];
